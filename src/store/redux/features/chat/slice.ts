72c3455103be71ee6e35ed58659e1f582ffdc3a0
hmy751 <hmy751@gmail.com>
2024-09-13 00:34:40 +0900

    refactor: chat state 수정

--- src/store/redux/features/chat/slice.ts
+++ src/store/redux/features/chat/slice.ts
@@ -1,34 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import type { StatusType } from "@/store/redux/type";
 
 interface ChatContent {
-  id: bigint;
+  id: bigint | null;
   speaker: "user" | "bot";
   content: string;
 }
 
 interface ChatState {
   contents: ChatContent[];
+  currentChat: {
+    status: StatusType;
+    id: bigint | null;
+    speaker: "user" | "bot";
+    content: string;
+  };
 }
 
 const initialState: ChatState = {
   contents: [],
+  currentChat: {
+    status: "idle",
+    id: null,
+    speaker: "user",
+    content: "",
+  },
 };
 
 const slice = createSlice({
   name: "chat",
   initialState,
   reducers: {
-    initializeChat: (state, action: PayloadAction<ChatContent[]>) => {
+    initializeChatState: (state, action: PayloadAction<ChatContent[]>) => {
       if (action.payload?.length) {
         state.contents = [...action.payload];
+      } else {
+        state.contents = [];
       }
+
+      state.currentChat = initialState.currentChat;
     },
-    addContent: (state, action: PayloadAction<ChatContent>) => {
-      state.contents.push(action.payload);
+    initializeCurrentChat: (state) => {
+      state.currentChat = initialState.currentChat;
     },
   },
 });
 
-export const { initializeChat, addContent } = slice.actions;
+export const { initializeChatState, initializeCurrentChat } = slice.actions;
 export default slice.reducer;
