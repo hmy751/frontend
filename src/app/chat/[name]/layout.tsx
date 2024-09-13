6cd70cdf40cade6e82c2b5d0d2d08b370742f29f
hmy751 <hmy751@gmail.com>
2024-09-13 14:57:54 +0900

    style: 레이아웃 크기 css 수정

--- src/app/chat/[name]/layout.tsx
+++ src/app/chat/[name]/layout.tsx
@@ -1,8 +1,8 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <Flex flexDirection={"column"} minHeight={"100vh"}>
       <Box
         as="header"
         width="100%"
@@ -12,12 +12,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <Box
         as="section"
         width="100%"
-        height="100%"
         display="flex"
+        flex={1}
         justifyContent={"center"}
       >
         {children}
       </Box>
-    </>
+    </Flex>
   );
 }
