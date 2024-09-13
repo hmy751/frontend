8e1446fad5a27035fa4bb18be8a3d3cec58076e0
diydriller <dhrhd080@naver.com>
2024-09-14 00:13:06 +0900

    feat: 면접관 정보 표시

--- src/app/interviewer/page.tsx
+++ src/app/interviewer/page.tsx
@@ -8,25 +8,34 @@ import Background from "./_components/Background";
 import Camera from "./_components/Camera";
 import InterviewerCard from "./_components/InterviewerCard";
 import { nomalizeIndex } from "./_utils/convert";
+import InterviewerInfo from "./_components/InterviewerInfo";
 
 const interviewerList = [
   {
     id: 1,
     imgUrl: "/images/ENFP.webp",
+    name: "민지",
+    mbti: "ENFP",
   },
   {
     id: 2,
     imgUrl: "/images/ESTJ.webp",
+    name: "철수",
+    mbti: "ESTJ",
   },
   {
     id: 3,
     imgUrl: "/images/ISFP.webp",
+    name: "영희",
+    mbti: "ISFP",
   },
 ];
 
 interface Interviewer {
   id: number;
+  name: string;
   imgUrl: string;
+  mbti: string;
 }
 
 const InterviewerChoicePage: React.FC = () => {
@@ -51,6 +60,7 @@ const InterviewerChoicePage: React.FC = () => {
       >
         <Camera />
         <Background imageUrl={selectedInterviewer?.imgUrl || ""} />
+        <InterviewerInfo selectedInterviewer={selectedInterviewer} />
         {interviewerList.map((interviewer, index) => (
           <InterviewerCard
             key={interviewer.id}
