2943a08264d48515f0c8740e6c8cabd04b18f636
hmy751 <hmy751@gmail.com>
2024-09-13 14:45:09 +0900

    style: 글로벌 css chakraProvider 제거 및 전체 box-sizing 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={noto_sans_kr.className}>
-        <ChakraProvider theme={theme}>
+        <ChakraProvider theme={theme} resetCSS={false}>
           <StoreProvider>{children}</StoreProvider>
         </ChakraProvider>
         <AudioPlayer />
