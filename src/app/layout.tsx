678c7f3eb793e021f1edc458773220fc7443b043
hmy751 <hmy751@gmail.com>
2024-09-23 16:37:55 +0900

    chore: msw, mockServiceWorker 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,23 @@
-"use client";
-
 import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 import ChakraProvider from "./_providers/ChakraProvider";
 import ReduxStoreProvider from "./_providers/ReduxStoreProvider";
 import { theme } from "@/styles/theme";
 import AudioPlayer from "../components/AudioPlayer";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import QueryProviders from "./_providers/QueryProvider";
+
+import { setupServer } from "msw/node";
+import { setupWorker } from "msw/browser";
+
+if (process.env.USE_MSW) {
+  if (typeof window === "undefined") {
+    const server = setupServer([]);
+    server.listen();
+  } else {
+    const worker = setupWorker([]);
+    worker.start();
+  }
+}
 
 const noto_sans_kr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -18,17 +29,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient();
-
   return (
     <html lang="en">
       <body className={noto_sans_kr.className}>
-        <QueryClientProvider client={queryClient}>
+        <QueryProviders>
           <ChakraProvider theme={theme} resetCSS={false}>
             <ReduxStoreProvider>{children}</ReduxStoreProvider>
           </ChakraProvider>
           <AudioPlayer />
-        </QueryClientProvider>
+        </QueryProviders>
       </body>
     </html>
   );
