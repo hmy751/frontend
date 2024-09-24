94c5b433b96c1b07cae49f171057375cc77d6380
hmy751 <hmy751@gmail.com>
2024-09-24 16:39:03 +0900

    refactor: client 요청을 핸들링 하도록 provider 형태로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import ReduxStoreProvider from "./_providers/ReduxStoreProvider";
 import { theme } from "@/styles/theme";
 import AudioPlayer from "../components/AudioPlayer";
 import QueryProviders from "./_providers/QueryProvider";
-import { MSWComponent } from "@/components/MSWComponent";
+import MSWProvider from "@/app/_providers/MSWProvider";
 
 const noto_sans_kr = Noto_Sans_KR({
   subsets: ["latin"],
@@ -17,16 +17,30 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isUseMsw = process.env.NEXT_PUBLIC_USE_MSW;
+
   return (
     <html lang="en">
       <body className={noto_sans_kr.className}>
-        <MSWComponent />
-        <QueryProviders>
-          <ChakraProvider theme={theme} resetCSS={false}>
-            <ReduxStoreProvider>{children}</ReduxStoreProvider>
-          </ChakraProvider>
-          <AudioPlayer />
-        </QueryProviders>
+        {isUseMsw ? (
+          <MSWProvider>
+            <QueryProviders>
+              <ChakraProvider theme={theme} resetCSS={false}>
+                <ReduxStoreProvider>{children}</ReduxStoreProvider>
+              </ChakraProvider>
+              <AudioPlayer />
+            </QueryProviders>
+          </MSWProvider>
+        ) : (
+          <>
+            <QueryProviders>
+              <ChakraProvider theme={theme} resetCSS={false}>
+                <ReduxStoreProvider>{children}</ReduxStoreProvider>
+              </ChakraProvider>
+              <AudioPlayer />
+            </QueryProviders>
+          </>
+        )}
       </body>
     </html>
   );
