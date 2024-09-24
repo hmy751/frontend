52c0b60d397ac195aae69a40d23b3f92a7e69c28
hmy751 <hmy751@gmail.com>
2024-09-24 20:47:53 +0900

    feat: postFn 함수 구현

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -37,3 +37,41 @@ export const get: GetFn = async ({ path, headers, body, externalUrl }) => {
     console.error(err);
   }
 };
+
+type PostFn = (arg: {
+  path?: string;
+  headers?: Record<string, string>;
+  body?: object;
+  externalUrl?: string;
+}) => Promise<Response | undefined>;
+
+export const post: PostFn = async ({ path, headers, body, externalUrl }) => {
+  try {
+    let url;
+
+    if (externalUrl) {
+      url = externalUrl;
+    } else {
+      url = `${baseURL}/${path}`;
+    }
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    };
+
+    const result = await fetch(url, options);
+    const data = await result.json();
+
+    if (!result.ok) {
+      throw Error("Http Error");
+    }
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
