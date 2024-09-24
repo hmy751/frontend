8c2a3e982008464e2d2a33321ff32b702770fdce
hmy751 <hmy751@gmail.com>
2024-09-24 20:01:40 +0900

    feat: get fetch 함수 작성

---
+++ src/apis/index.ts
@@ -0,0 +1,39 @@
+const baseURL = process.env.NEXT_PUBLIC_API_HOST;
+
+type GetFn = (arg: {
+  path?: string;
+  headers?: Record<string, string>;
+  body?: object;
+  externalUrl?: string;
+}) => Promise<Response | undefined>;
+
+export const get: GetFn = async ({ path, headers, body, externalUrl }) => {
+  try {
+    let url;
+
+    if (externalUrl) {
+      url = externalUrl;
+    } else {
+      url = `${baseURL}/${path}`;
+    }
+
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: JSON.stringify(body),
+    };
+
+    const result = await fetch(url, options);
+    const data = await result.json();
+
+    if (!result.ok) {
+      throw Error("Http Error");
+    }
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
