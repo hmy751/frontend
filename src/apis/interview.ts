ad5f70e9d488fee035adc652e80846bb9484ef5c
hmy751 <hmy751@gmail.com>
2024-09-26 02:43:45 +0900

    refactor: 채팅 api 로직 분리

--- src/apis/interview.ts
+++ src/apis/interview.ts
@@ -19,3 +19,48 @@ export const fetchInterview = async ({
     },
   });
 };
+
+interface SpeechToTextProps {
+  formData: FormData;
+}
+export interface SpeechToTextData {
+  text: string;
+}
+
+export const fetchSpeechToText = async ({
+  formData,
+}: SpeechToTextProps): Promise<SpeechToTextData> => {
+  try {
+    const result = await fetch("/api/chat", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data: SpeechToTextData = await result.json();
+    if (!result.ok) {
+      throw Error("Http Error");
+    }
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+interface AIChatProps {
+  chatId: number;
+  content: string;
+}
+
+export interface AIChatData {
+  content: string;
+}
+
+export const fetchAIChat = async ({ chatId, content }: AIChatProps) => {
+  return post<AIChatData>({
+    path: `/interview/${chatId}/contents`,
+    body: {
+      content,
+    },
+  });
+};
